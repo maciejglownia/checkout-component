2e48041f43c62b72231fefec6192b97f82563bd5
Maciej Głownia <maciej.k.glownia@gmail.com>
2024-12-16 18:55:26 +0100

    Initial commit

---
+++ src/main/java/pl/glownia/maciej/checkoutcomponent/CheckoutComponentApplication.java
@@ -0,0 +1,13 @@
+package pl.glownia.maciej.checkoutcomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutComponentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/glownia/maciej/checkoutcomponent/CheckoutComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.glownia.maciej.checkoutcomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
