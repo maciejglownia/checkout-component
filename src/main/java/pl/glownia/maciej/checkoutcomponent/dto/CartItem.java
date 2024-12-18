d49b9fdfaecd0a0859f005d3988dda4aec1e8a94
Maciej Głownia <maciej.k.glownia@gmail.com>
2024-12-18 23:28:35 +0100

    Add dtos for item, cart item

---
+++ src/main/java/pl/glownia/maciej/checkoutcomponent/dto/CartItem.java
@@ -0,0 +1,36 @@
+package pl.glownia.maciej.checkoutcomponent.dto;
+
+public class CartItem {
+    private Item item;
+    private int quantity;
+
+    public CartItem(Item item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "item=" + item +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+}
